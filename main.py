cf99d236d467670d56a3e8eb6b025162aa73faf6
Takuya Tokuda <cs.toku.mail@gmail.com>
2019-03-04 04:22:44 +0900

    update

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ repo = Repo('./website')
 origin = repo.remote()
 #origin.fetch()
 
-print(origin.refs.master)
 head_ref = None
 for ref in origin.refs:
     if ref.remote_head == 'dev-1.13-ja.1':
@@ -22,18 +21,16 @@ bc = base_commit
 hc = head_commit
 while not bc == hc:
     if bc.committed_date < hc.committed_date:
-        #print('{}({} > {}): {}'.format(head_ref, bc.committed_date, hc.committed_date, hc))
         hc = hc.parents[0]
     else:
-        #print('{}({} < {}): {}'.format(origin.refs.master, bc.committed_date, hc.committed_date, bc))
         bc = bc.parents[0]
 
 branch_point = bc
 base_diff = branch_point.diff(base_commit, create_patch=True)
 
-for obj in base_diff[:10]:
-    print(obj.change_type)
-    print('Path: {} => {}'.format(obj.a_path, obj.b_path))
-    print('Blob: {} => {}'.format(obj.a_blob, obj.b_blob))
-    print(obj.diff.decode())
+l10n_contents = [blob.path.replace('content/ja/', '', 1) for blob in head_commit.tree.list_traverse() if blob.path.startswith('content/ja/')]
+
+for obj in base_diff:
+    if obj.a_path and obj.a_path.startswith('content/en/'):
+        print('{} => {}'.format(obj.a_path, obj.b_path))
 
